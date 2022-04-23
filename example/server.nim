05029bbb992751564b8904c17fad91355fa61721
obemaru4012 <on.kato@protonmail.com>
2022-04-23 16:00:06 +0900

    【0.2.0】サンプルコードの修正中

--- example/server.nim
+++ example/server.nim
@@ -9,11 +9,12 @@ import asyncdispatch,
 
 from ../bamboo_websocket/connection_status import ConnectionStatus
 from ../bamboo_websocket/opcode import Opcode
-from ../bamboo_websocket/websocket import WebSocket, WebSocketC
+from ../bamboo_websocket/websocket import WebSocket, WebSockets, WebSocketC
 from ../bamboo_websocket/receive_result import ReceiveResult
 from ../bamboo_websocket/bamboo_websocket import handshake, loadServerSetting, openWebSocket, receiveMessage, sendMessage
 
 var setting = loadServerSetting()
+var websockets = WebSockets
 
 proc callBack(request: Request) {.async, gcsafe.} =
   var ws = WebSocket()
@@ -23,17 +24,29 @@ proc callBack(request: Request) {.async, gcsafe.} =
     var ws: WebSocket
     try:
       ws = await openWebSocket(request, setting)
+      websockets.add(ws)
     except:
       discard
 
     while ws.status == ConnectionStatus.OPEN:
       try:
         let receive = await ws.receiveMessage()
-        echo(counter, ": ", receive.OPCODE, "=" ,receive.MESSAGE)
-        counter += 1
+        if receive.OPCODE == OpCode.TEXT:
+          echo(counter, ": ", receive.OPCODE, "=" , receive.MESSAGE)
+          counter += 1
+
+        if receive.OPCODE == OpCode.CLOSE:
+          echo("ID: ", ws.id, " has Closed.")
+
+        for websocket in websockets:
+          if websocket.id != ws.id and receive.OPCODE == OpCode.TEXT:
+            await websocket.sendMessage(receive.MESSAGE, 0x1, 3000, true)
+
       except:
         ws.status = ConnectionStatus.CLOSED
         ws.socket.close()
+ 
+    websockets.delete(websockets.find(ws))
 
 if isMainModule:
   var server = newAsyncHttpServer()
