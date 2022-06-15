8f8969115e55fdaf43101fbf0641931ffb4f55d1
obemaru4012 <on.kato@protonmail.com>
2022-06-15 16:47:30 +0900

    【0.3.0】WebSockets, WebSocketsGroupsオブジェクトを削除。

--- example/chat_example/chat_server.nim
+++ example/chat_example/chat_server.nim
@@ -11,11 +11,12 @@ import asyncdispatch,
 
 from bamboo_websocket/connection_status import ConnectionStatus
 from bamboo_websocket/opcode import Opcode
-from bamboo_websocket/websocket import WebSocket, WebSockets, WebSocketC
+from bamboo_websocket/websocket import WebSocket
 from bamboo_websocket/receive_result import ReceiveResult
 from bamboo_websocket/bamboo_websocket import handshake, loadServerSetting, openWebSocket, receiveMessage, sendMessage
 
 var setting = loadServerSetting()
+var WebSockets: seq[WebSocket] = newSeq[WebSocket]()
 
 proc subProtcolsProc(ws: WebSocket, sub_protocol: seq[string]): bool {.gcsafe.} = 
   ##[
